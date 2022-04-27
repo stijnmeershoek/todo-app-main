32d565da1d1c7cce646212addeb958b0c26361c8
ROGU3N1NJA <smeershoek2006@gmail.com>
2022-04-27 12:42:30 +0200

    added new vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: "/todo-app-main/",
+  build: {
+    target: 'es2019',
+    outDir: 'build'
+  },
+  plugins: [react()],
 })
